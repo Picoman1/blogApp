79aeb5125c3e1824fe0199d89558c2725939e1bc
Nikhil Francis <nikhilfrancis2001@gamil.com>
2025-01-23 16:28:34 +0530

    env file added

--- day-28 blog back end/connection/db.js
+++ day-28 blog back end/connection/db.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose')
 
-mongoose.connect('mongodb+srv://Nikhil:iamsorry1@cluster0.lylxk.mongodb.net/BlogApp?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+mongoose.connect(process.env.mongoUrl).then(()=>{
     console.log('database connected');
 }).catch(()=>{
     console.log('database connection failed')
